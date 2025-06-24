934f5455433c71cf5df87c2bb261745d8bb8032f
Sahil Singh <sahilhr24@gmail.com>
2025-06-24 16:17:14 +0530

    v5594

--- Demo.sh
+++ Demo.sh
@@ -8,10 +8,10 @@
 24-06-2025  11:33    <DIR>          Pipeline-For_Cloud_RUN
 24-06-2025  12:04               370 Pipeline-For_Cloud_RUN.bat
 24-06-2025  11:42    <DIR>          Pipeline-For_GCP_VM
-24-06-2025  11:41               371 Pipeline-For_GCP_VM.bat
+24-06-2025  12:04               364 Pipeline-For_GCP_VM.bat
 24-06-2025  11:43    <DIR>          Pipeline-For_Private_GKE_Cluster
-24-06-2025  11:43               397 Pipeline-For_Private_GKE_Cluster.bat
+24-06-2025  12:04               390 Pipeline-For_Private_GKE_Cluster.bat
 24-06-2025  11:43    <DIR>          Pipeline-For_Public_GKE_Cluster
-24-06-2025  11:43               395 Pipeline-For_Public_GKE_Cluster.bat
-               4 File(s)          1,533 bytes
-               6 Dir(s)  189,881,061,376 bytes free
+24-06-2025  12:05               388 Pipeline-For_Public_GKE_Cluster.bat
+               4 File(s)          1,512 bytes
+               6 Dir(s)  189,881,044,992 bytes free
