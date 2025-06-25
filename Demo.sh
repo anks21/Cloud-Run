1584a2a39b9ff60e40a1991a1eaa7087ee872cc7
Sahil Singh <sahilhr24@gmail.com>
2025-06-25 11:18:03 +0530

    v8744

--- Demo.sh
+++ Demo.sh
@@ -3,15 +3,15 @@
 
  Directory of D:\1. GIT
 
-24-06-2025  11:26    <DIR>          .
-24-06-2025  11:26    <DIR>          ..
-24-06-2025  11:33    <DIR>          Pipeline-For_Cloud_RUN
-24-06-2025  12:04               370 Pipeline-For_Cloud_RUN.bat
-24-06-2025  11:42    <DIR>          Pipeline-For_GCP_VM
-24-06-2025  12:04               364 Pipeline-For_GCP_VM.bat
-24-06-2025  11:43    <DIR>          Pipeline-For_Private_GKE_Cluster
-24-06-2025  12:04               390 Pipeline-For_Private_GKE_Cluster.bat
-24-06-2025  11:43    <DIR>          Pipeline-For_Public_GKE_Cluster
-24-06-2025  12:05               388 Pipeline-For_Public_GKE_Cluster.bat
+24-06-2025  11:26 AM    <DIR>          .
+24-06-2025  11:26 AM    <DIR>          ..
+24-06-2025  11:33 AM    <DIR>          Pipeline-For_Cloud_RUN
+24-06-2025  12:04 PM               370 Pipeline-For_Cloud_RUN.bat
+24-06-2025  11:42 AM    <DIR>          Pipeline-For_GCP_VM
+24-06-2025  12:04 PM               364 Pipeline-For_GCP_VM.bat
+24-06-2025  11:43 AM    <DIR>          Pipeline-For_Private_GKE_Cluster
+24-06-2025  12:04 PM               390 Pipeline-For_Private_GKE_Cluster.bat
+24-06-2025  11:43 AM    <DIR>          Pipeline-For_Public_GKE_Cluster
+24-06-2025  12:05 PM               388 Pipeline-For_Public_GKE_Cluster.bat
                4 File(s)          1,512 bytes
-               6 Dir(s)  189,881,761,792 bytes free
+               6 Dir(s)  189,881,753,600 bytes free
